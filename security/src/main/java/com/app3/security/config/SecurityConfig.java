e9d5604d41d71274d566312074174197c7fce7bd
Steven <memora182@soy.sena.edu.co>
2024-05-08 14:00:33 -0500

    añadir cambios

---
+++ security/src/main/java/com/app3/security/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.app3.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.HttpBasicDsl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity  httpSecurity) throws Exception {
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPaswordEncoder (null);
+        provider.setUserDetailsService (null);
+        return provider;
+    }
+
+
+    @Bean
+    public PasswordEncoder  passwordEncoder (){
+        return No0pPasswordEncoder.getInstance();
+    }
+}
---
+++ security/src/main/java/com/app3/security/controller/TestAuthController.java
@@ -0,0 +1,27 @@
+package com.app3.security.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+
+public class TestAuthController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world ";
+    }
+
+    @GetMapping("/hello-secured")
+    public String helloSecured(){
+        return "Hello World Secured ";
+    }
+
+
+}
+
+
+
